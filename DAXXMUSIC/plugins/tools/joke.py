f39f5cd6da531b6e93ed80f515d1674c69a99330
Dds2 <85358022+dds256@users.noreply.github.com>
2024-05-31 17:53:53 +0530

    Update joke.py

--- DAXXMUSIC/plugins/tools/joke.py
+++ DAXXMUSIC/plugins/tools/joke.py
@@ -1,12 +1,25 @@
-import requests
-from DAXXMUSIC import app
-from pyrogram import Client, filters
+import aiohttp
+from pyrogram import filters
+from DAXXMUSIC import app as app
 
-JOKE_API_ENDPOINT = 'https://hindi-jokes-api.onrender.com/jokes?api_key=1a6d440e3f5971eecebceee818c2'
+# Function to get a random joke
+async def get_joke():
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://v2.jokeapi.dev/joke/Any") as response:
+            if response.status == 200:
+                return await response.json()
+            return None
 
-@app.on_message(filters.command("hjoke"))
-async def joke(_, message):
-    response = requests.get(JOKE_API_ENDPOINT)
-    r = response.json()
-    joke_text = r['jokeContent']
-    await message.reply_text(joke_text)
+@app.on_message(filters.command("joke"))
+async def joke_command(bot, message):
+    processing_message = await message.reply("Fetching a joke...")
+    try:
+        joke_data = await get_joke()
+        if not joke_data:
+            await processing_message.edit("Failed to get a joke. Please try again later.")
+            return
+        
+        joke = joke_data.get("joke") if "joke" in joke_data else f"{joke_data.get('setup')} - {joke_data.get('delivery')}"
+        await processing_message.edit(joke)
+    except Exception as e:
+        await processing_message.edit(f"An error occurred: {str(e)}")
