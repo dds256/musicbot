4fff0c94403078ee736f0490e1fe1983796b0d2f
Dds2 <85358022+dds256@users.noreply.github.com>
2024-06-04 15:21:32 +0530

    Update upscale.py

--- DAXXMUSIC/plugins/tools/upscale.py
+++ DAXXMUSIC/plugins/tools/upscale.py
@@ -1,25 +1,21 @@
 import base64
 import httpx
 import os
-import requests 
-from pyrogram import filters
+from pyrogram import filters, Client
 from config import BOT_USERNAME
 from DAXXMUSIC import app
-from pyrogram import filters
-import pyrogram
 from uuid import uuid4
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
-
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @app.on_message(filters.reply & filters.command("upscale"))
-async def upscale_image(app, message):
+async def upscale_image(client, message):
     try:
         if not message.reply_to_message or not message.reply_to_message.photo:
-            await message.reply_text("ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ.")
+            await message.reply_text("Please reply to an image to upscale it.")
             return
 
         image = message.reply_to_message.photo.file_id
-        file_path = await app.download_media(image)
+        file_path = await client.download_media(image)
 
         with open(file_path, "rb") as image_file:
             f = image_file.read()
@@ -28,23 +24,33 @@ async def upscale_image(app, message):
 
         async with httpx.AsyncClient() as http_client:
             response = await http_client.post(
-                "https://api.qewertyy.me/upscale", data={"image_data": b}, timeout=None
+                "https://api.qewertyy.me/upscale", json={"image_data": b}, timeout=None
             )
 
-        with open("upscaled.png", "wb") as output_file:
+        if response.status_code != 200:
+            await message.reply_text("Failed to upscale the image. Please try again later.")
+            return
+
+        upscaled_image_path = "upscaled.png"
+        with open(upscaled_image_path, "wb") as output_file:
             output_file.write(response.content)
 
         await client.send_document(
             message.chat.id,
-            document="upscaled.png",
-            caption="ʜᴇʀᴇ ɪs ᴛʜᴇ ᴜᴘsᴄᴀʟᴇᴅ ɪᴍᴀɢᴇ!",
+            document=upscaled_image_path,
+            caption="Here is the upscaled image!"
         )
 
+        os.remove(file_path)
+        os.remove(upscaled_image_path)
+
     except Exception as e:
-        print(f"ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ: {e}")
-        await message.reply_text("ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
+        print(f"Failed to upscale the image: {e}")
+        await message.reply_text("Failed to upscale the image. Please try again later.")
 
 # ------------
+
+import requests
 
 waifu_api_url = 'https://api.waifu.im/search'
 
@@ -76,3 +82,4 @@ def waifu_command(client, message):
 
     except Exception as e:
         message.reply_text(f"An error occurred: {str(e)}")
+        
