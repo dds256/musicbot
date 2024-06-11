ffa79e0cda527b2d3e2e6a4c936d25b0d95b2b4f
Dds2 <85358022+dds256@users.noreply.github.com>
2024-06-11 20:11:34 +0530

    Update fakeinfo.py

--- DAXXMUSIC/plugins/Yumi/fakeinfo.py
+++ DAXXMUSIC/plugins/Yumi/fakeinfo.py
@@ -1,43 +1,21 @@
-"""***
-MIT License
-
-Copyright (c) [2023] [DAXX TEAM]
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.***
-"""
-
 import requests
 from DAXXMUSIC import app
 from pyrogram import filters
 
-
-@app.on_message(filters.command("fake"))
-async def address(_, message):
-    query = message.text.split(maxsplit=1)[1].strip()
+@app.on_message(filters.command("id_faker"))
+async def address(client, message):
+    user = message.from_user.first_name
+    query = message.text.split(maxsplit=1)[1].strip() if len(message.text.split(maxsplit=1)) > 1 else "us"
     url = f"https://randomuser.me/api/?nat={query}"
+    
+    process_message = await message.reply_text("Generating fake identity, please wait...")
+
     response = requests.get(url)
     data = response.json()
 
     if "results" in data:
         user_data = data["results"][0]
 
-        
         name = f"{user_data['name']['title']} {user_data['name']['first']} {user_data['name']['last']}"
         address = f"{user_data['location']['street']['number']} {user_data['location']['street']['name']}" 
         city = user_data['location']['city']
@@ -48,20 +26,39 @@ async def address(_, message):
         phone = user_data['phone']
         picture_url = user_data['picture']['large']
 
-        
-        caption = f"""
-﹝⌬﹞**ɴᴀᴍᴇ** ⇢ {name}
-﹝⌬﹞**ᴀᴅᴅʀᴇss** ⇢ {address}
-﹝⌬﹞**ᴄᴏᴜɴᴛʀʏ** ⇢ {country}
-﹝⌬﹞**ᴄɪᴛʏ** ⇢ {city}
-﹝⌬﹞**sᴛᴀᴛᴇ** ⇢ {state}
-﹝⌬﹞**ᴘᴏsᴛᴀʟ** ⇢ {postal}
-﹝⌬﹞**ᴇᴍᴀɪʟ** ⇢ {email}
-﹝⌬﹞**ᴘʜᴏɴᴇ** ⇢ {phone}
+        # Additional Information
+        username = user_data['login']['username']
+        dob = user_data['dob']['date']
+        gender = user_data['gender']
+        nationality = user_data['nat']
+        registered = user_data['registered']['date']
+        cell = user_data['cell']
+        id_name = user_data['id']['name']
+        id_value = user_data['id']['value']
 
+        caption = f"""
+**{user}, here's your fake identity:**
+
+**ɴᴀᴍᴇ** ⇢ `{name}`
+**ᴀᴅᴅʀᴇss** ⇢ `{address}`
+**ᴄᴏᴜɴᴛʀʏ** ⇢ `{country}`
+**ᴄɪᴛʏ** ⇢ `{city}`
+**sᴛᴀᴛᴇ** ⇢ `{state}`
+**ᴘᴏsᴛᴀʟ** ⇢ `{postal}`
+**ᴇᴍᴀɪʟ** ⇢ {email}
+**ᴘʜᴏɴᴇ** ⇢ {phone}
+**ᴜsᴇʀɴᴀᴍᴇ** ⇢ `{username}`
+**ᴅᴀᴛᴇ ᴏғ ʙɪʀᴛʜ** ⇢ `{dob}`
+**ɢᴇɴᴅᴇʀ** ⇢ `{gender}`
+**ɴᴀᴛɪᴏɴᴀʟɪᴛʏ** ⇢ `{nationality}`
+**ʀᴇɢɪsᴛᴇʀᴇᴅ** ⇢ `{registered}`
+**ᴄᴇʟʟ** ⇢ {cell}
+**ID ({id_name})** ⇢ `{id_value}`
         """
 
-        
+        await process_message.delete()
         await message.reply_photo(photo=picture_url, caption=caption)
     else:
+        await process_message.delete()
         await message.reply_text("ᴏᴏᴘs ɴᴏᴛ ғᴏᴜɴᴅ ᴀɴʏ ᴀᴅᴅʀᴇss.")
+        
